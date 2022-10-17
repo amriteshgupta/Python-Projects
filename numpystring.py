b314911b60c160195c1b1ce2c2bc7311b2ca7075
amaldbbd <108591103+amaldbbd@users.noreply.github.com>
2022-10-17 11:41:42 +0530

    create a numoystring file
    Added a numoystring file

---
+++ numpystring.py
@@ -0,0 +1,10 @@
+import numpy as np
+x1 = np.array(['Python', 'PHP'], dtype=np.str)
+x2 = np.array([' Java', ' C++'], dtype=np.str)
+print("Array1:")
+print(x1)
+print("Array2:")
+print(x2)
+new_array = np.char.add(x1, x2)
+print("new array:")
+print(new_array)
